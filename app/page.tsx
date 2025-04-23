74bf6da08c9e212e1ac68987566b2cd292209a9b
moharamxr <haramm058@gmail.com>
2025-04-23 02:45:33 +0200

    feat(SEO): Improve site indexability and social media sharing metadata

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,12 @@ import Products from "@/components/products/Products";
 export default function Home() {
   return (
     <div className="flex justify-start items-center flex-col px-10 py-7 min-h-screen">
-      <h2
+      <h1
         className="font-normal text-[42px] leading-[32px] tracking-normal text-center align-middle capitalize"
         style={{ fontFamily: "Volkhov" }}
       >
         Home
-      </h2>
+      </h1>
       <Products />
     </div>
   );
